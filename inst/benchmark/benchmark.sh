706e170fdec88c0503b966272f4b5360d81016ca
Teemu Rintala <teemu.rintala.a@gmail.com>
2021-04-22 09:55:32 +0300

    Batch script

---
+++ inst/benchmark/benchmark.sh
@@ -0,0 +1,67 @@
+# This runs all the R scripts to generate results included in the patient stratification article
+# TODO: create all the necessary folders in bash or R 
+#       e.g. dir.create(..., recursive = TRUE, showWarnings = FALSE).
+# TODO: read folders from configuration
+
+## TCGA breast cancer
+# No gene filters DR
+Rscript brca/brca_dimred.R --no-restore --no-save
+# DEG and OTP intersection DR
+Rscript brca/brca_otp_dimred.R --no-restore --no-save
+# DEG and OTP intersection GSVA
+Rscript brca/brca_otp_gsva.R --no-restore --no-save
+# DEG and OTP intersection DiffRank
+Rscript brca/brca_otp_diffrank.R --no-restore --no-save
+# DEG and OTP intersection RWRFGSEA on GCN
+Rscript brca/rwr/brca_rwr_gcn_fixedn.R --no-restore --no-save
+Rscript --no-restore --no-save brca/rwr/rwr_clustering_only.R \
+  ~/tcga/brca/intermediary_files/rwr/gcn_fixedn/ \
+  rwr.pw.scores.csv rwr.pw.clusters.csv.gz \
+  KEGG.csv.gz GO.csv.gz REACTOME.csv.gz
+# DEG and OTP intersection RWRFGSEA on PPI
+Rscript brca/rwr/brca_rwr_ppi_fixedn.R --no-restore --no-save
+Rscript --no-restore --no-save brca/rwr/rwr_clustering_only.R \
+  ~/tcga/brca/intermediary_files/rwr/ppi_fixedn/ \
+  rwr.pw.scores.csv rwr.pw.clusters.csv.gz \
+  KEGG.csv.gz GO.csv.gz REACTOME.csv.gz
+
+## PRAD breast cancer
+# No gene filters DR
+Rscript prad/prad_dimred.R --no-restore --no-save
+# DEG and OTP intersection DR
+Rscript prad/prad_otp_dimred.R --no-restore --no-save
+# DEG and OTP intersection GSVA
+Rscript prad/prad_otp_gsva.R --no-restore --no-save
+# DEG and OTP intersection DiffRank
+Rscript prad/prad_otp_diffrank.R --no-restore --no-save
+# DEG and OTP intersection RWRFGSEA on GCN
+Rscript prad/rwr/prad_rwr_gcn_fixedn.R --no-restore --no-save
+Rscript --no-restore --no-save prad/rwr/rwr_clustering_only.R \
+  ~/tcga/prad/intermediary_files/rwr/gcn_fixedn/ \
+  rwr.pw.scores.csv rwr.pw.clusters.csv.gz \
+  KEGG.csv.gz GO.csv.gz REACTOME.csv.gz
+# DEG and OTP intersection RWRFGSEA on PPI
+Rscript prad/rwr/prad_rwr_ppi_fixedn.R --no-restore --no-save
+Rscript --no-restore --no-save prad/rwr/rwr_clustering_only.R \
+  ~/tcga/prad/intermediary_files/rwr/ppi_fixedn/ \
+  rwr.pw.scores.csv rwr.pw.clusters.csv.gz \
+  KEGG.csv.gz GO.csv.gz REACTOME.csv.gz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ inst/benchmark/conf.R
@@ -0,0 +1,4 @@
+# Specify location to save clustering results to
+path_intermediate_results <- "~/tcga/"
+path_plots <- "~/tcga/plot"
+
