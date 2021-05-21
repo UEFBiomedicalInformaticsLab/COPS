ef16b10140018e5a022ded33092cb0a82d8bd6e6
Teemu Rintala <teemu.rintala.a@gmail.com>
2021-05-21 11:39:18 +0300

    added hallmark gene sets

--- inst/benchmark/brca/brca_default_annotations.R
+++ inst/benchmark/brca/brca_default_annotations.R
@@ -1,5 +1,8 @@
 # Load the functional annotations used by GSE scripts
-db_annots <- msigdbr::msigdbr(species = "Homo sapiens")
-db_annots <- dplyr::filter(db_annots, grepl(paste(GENE_SETS, collapse = "|"), gs_subcat))
+db_annots <- msigdbr::msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CP:KEGG")
+db_annots <- rbind(db_annots, msigdbr::msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CP:REACTOME"))
+db_annots <- rbind(db_annots, msigdbr::msigdbr(species = "Homo sapiens", category = "C5", subcategory = "GO:BP"))
+db_annots <- rbind(db_annots, msigdbr::msigdbr(species = "Homo sapiens", category = "H"))
+
 list_db_annots <- lapply(split(db_annots, db_annots$gs_name), function(x) x$gene_symbol)
 list_db_annots <- list_db_annots[which(sapply(list_db_annots, length) <= 200 & sapply(list_db_annots, length) >= 5)]
